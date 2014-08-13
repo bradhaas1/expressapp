6c14246f509a9f4472f0714cebc93addd507613d
Brad Haas <brad.haas@nbcuni.com>
2014-08-13 15:39:19 -0700

    grunt tasking

--- gruntfile.js
+++ gruntfile.js
@@ -1,7 +1,25 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json')
+    // Configurable paths
+
+
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build:{
+        src: 'src/<%= pkg.name %>.js',
+        dst: 'build/<%= pkg.name %>.min.js'
+      }
+    }
   });
+
+  // load the plugin that supplies uglify task
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // register default task
+  grunt.registerTask('default', ['uglify']);
 
 
 };
